caa1dabdac4c013c2fe5c714572702256512dac3
Jordan Cohen <jordan1158@gmail.com>
2017-08-14 15:39:19 -0400

    added user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
+  
+  validates :username, uniqueness: {case_sensitive: false}, presence: true, length: {minimum: 3, maximum: 25}#ensures that the username column is present
+  
+  VALID_EMAIL_REGEX =  /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, uniqueness: {case_sensitive: false}, presence: true, length: {maximum: 105}, format: {with: VALID_EMAIL_REGEX}
+  
   has_many :articles
   has_many :comments
+  
 end
