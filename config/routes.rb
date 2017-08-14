6df4aed329c8abcaa14cd5d196992312271a99f3
Jordan Cohen <jordan1158@gmail.com>
2017-08-14 19:14:06 -0400

    Sign up users from ui added

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   resources :comments
-  resources :users
+  
+  #USERS ROUTES
+  resources :users, except: [:new]
+  get 'signup', to: 'users#new'
+  
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "articles#index"
   get "articles/list", 'articles#list'
+  post 'users', to: 'users#create'
   
   resources :articles do
   end
