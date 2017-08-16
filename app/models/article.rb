3d08053c9ba286871bb1d9f6b18668875949124a
Jordan Cohen <jordan1158@gmail.com>
2017-08-16 18:44:48 -0400

    unit testing

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,5 @@
 class Article < ApplicationRecord
   belongs_to :user
-  belongs_to :category
   validates :title, presence: true, length: {minimum: 5}
   validates :user_id, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,24 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170816195407) do
+ActiveRecord::Schema.define(version: 20170816221031) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
     t.integer  "num"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text     "language"
     t.integer  "user_id"
-    t.integer  "category_id"
-    t.index ["category_id"], name: "index_articles_on_category_id"
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "comments", force: :cascade do |t|
